fb72648e10e3e7c82aa5fdda630ba401345ff176
Timothy Smith <timsmith78@yahoo.com>
2018-09-28 19:28:36 -0500

    React router refactor

--- client/src/components/ResultEntry.js
+++ client/src/components/ResultEntry.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import moment from 'moment'
 
-const ResultEntry = props => 
+const ResultEntry = props =>
     <li>
-    <a href={props.entry.web_url}>{props.entry.headline.main}</a>
-    <p>{moment(props.entry.pub_date).format('LL')}</p>
-    <p>Abstract: {props.entry.abstract || 'Not Available'}</p>
+        <a href={props.entry.web_url} target="_blank">{props.entry.headline.main}</a>
+        <p>{moment(props.entry.pub_date).format('LL')}</p>
+        <p>Abstract: {props.entry.abstract || 'Not Available'}</p>
     </li>
 
 
